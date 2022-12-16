099212239f77896e26b40b85875202e9e322449f
abiodunraheem <raheemabiodun23@gmail.com>
2022-12-16 11:44:16 +0100

    Refactor configureStore.js file

--- src/redux/mission/missionListReducer.js
+++ src/redux/mission/missionListReducer.js
@@ -1,3 +1,41 @@
+// import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+// import axios from 'axios';
+
+// const initialState = {
+//   loading: false,
+//   error: '',
+//   missions: [],
+// };
+
+// export const fetchMissions = createAsyncThunk('fetchMissions', async () => {
+//   const response = await axios
+//     .get('https://api.spacexdata.com/v3/missions');
+//   return response.data;
+// });
+
+// const missionSlice = createSlice({
+//   name: 'mission',
+//   initialState,
+//   extraReducers: (builder) => {
+//     builder.addCase(fetchMissions.pending, (state) => {
+//       state.loading = true;
+//     });
+//     builder.addCase(fetchMissions.fulfilled, (state, action) => {
+//       state.loading = false;
+//       state.missions = action.payload;
+//       state.error = '';
+//     });
+//     builder.addCase(fetchMissions.rejected, (state, action) => {
+//       state.loading = false;
+//       state.missions = [];
+//       state.error = action.error.message;
+//     });
+//   },
+// });
+
+// const missionReducer = missionSlice.reducer;
+// export default missionReducer;
+
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
 

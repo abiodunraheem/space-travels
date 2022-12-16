df0d7fb3db90ff2c4b7b23e3d7fb3783bc6a39b8
abiodunraheem <raheemabiodun23@gmail.com>
2022-12-16 11:45:31 +0100

    Update rocketReducer function

--- src/redux/rocket/rocketListReducer.js
+++ src/redux/rocket/rocketListReducer.js
@@ -1,42 +1,3 @@
-// import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-// import axios from 'axios';
-
-// const initialState = {
-//   loading: false,
-//   error: '',
-//   rockets: [],
-// };
-
-// export const fetchRockets = createAsyncThunk('fetchRockets', async () => {
-//   const response = await axios
-//     .get('https://api.spacexdata.com/v3/rockets');
-//   return response.data;
-// });
-
-// const rocketSlice = createSlice({
-//   name: 'rocket',
-//   initialState,
-//   // reducers: {},
-//   extraReducers: (builder) => {
-//     builder.addCase(fetchRockets.pending, (state) => {
-//       state.loading = true;
-//     });
-//     builder.addCase(fetchRockets.fulfilled, (state, action) => {
-//       state.loading = false;
-//       state.rockets = action.payload;
-//       state.error = '';
-//     });
-//     builder.addCase(fetchRockets.rejected, (state, action) => {
-//       state.loading = false;
-//       state.rockets = [];
-//       state.error = action.error.message;
-//     });
-//   },
-// });
-
-// const rocketReducer = rocketSlice.reducer;
-// export default rocketReducer;
-
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
 
@@ -55,6 +16,7 @@ export const fetchRockets = createAsyncThunk('fetchRockets', async () => {
 const rocketSlice = createSlice({
   name: 'rocket',
   initialState,
+  // reducers: {},
   extraReducers: (builder) => {
     builder.addCase(fetchRockets.pending, (state) => {
       state.loading = true;

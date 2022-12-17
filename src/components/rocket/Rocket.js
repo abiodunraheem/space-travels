7e4fce32a7e89e11655eb078e5469ce2fb814a1c
abiodunraheem <raheemabiodun23@gmail.com>
2022-12-17 21:58:03 +0100

    Add rocket reservation button

--- src/components/rocket/Rocket.js
+++ src/components/rocket/Rocket.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchRockets } from '../../redux/rocket/rocketListReducer';
+import './rocket.css';
 
 function Rocket() {
   const rockets = useSelector((state) => state.rockets.rockets);
@@ -11,21 +12,28 @@ function Rocket() {
   return (
     <div>
       {rockets.map((item) => (
-        <div className="details-container" key={item.id}>
-          <div className="contents">
-            <div className="rocket_image">
-              <img src={item.flickr_images} alt="" />
-            </div>
-            <div className="mission_name">
-              <h3>{item.rocket_name}</h3>
-            </div>
-
-            <div className="description">
-              <h4>{item.description}</h4>
-            </div>
-            <div className="rocket_button">
-              <button type="submit">Add Rocket</button>
-            </div>
+        <div className="rocket" key={item.id}>
+          <div className="rocketLeft">
+            <img src={item.flickr_images} alt="" />
+          </div>
+          <div className="rocketRight">
+            <h2>{item.rocket_name}</h2>
+            {rockets.reserved
+              ? (
+                <>
+                  <p>
+                    <small className="reservedbtn">Reserved</small>
+                    {rockets.description}
+                  </p>
+                  <button className="cancelReserve" id={item.id} type="button">Cancel Reservation</button>
+                </>
+              )
+              : (
+                <>
+                  <p>{item.description}</p>
+                  <button id={item.id} type="button">Reserve Rocket</button>
+                </>
+              )}
           </div>
         </div>
       ))}
